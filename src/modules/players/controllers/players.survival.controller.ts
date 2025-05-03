58281b491867d48fdf7f8cdb97ecac84c54009e8
stephanofer <slowdown_4@hotmail.com>
2025-05-03 00:38:09 -0500

    refactor(players): remove unused player controllers and services for production

--- src/modules/players/controllers/players.survival.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Controller, Get, Param } from '@nestjs/common';
-
-@Controller('survi21/players')
-export class PlayersSurvivalController {
-  constructor() {}
-}
--- src/modules/players/players.module.ts
+++ src/modules/players/players.module.ts
@@ -1,21 +1,12 @@
 import { Module } from '@nestjs/common';
-import { PlayersService } from './services/players.service';
-import { PlayersController } from './controllers/players.controller';
-import { PlayersRPGController } from './controllers/players.rpg.controller';
-import { PlayersSurvivalController } from './controllers/players.survival.controller';
-import { PlayersRPGService } from './services/players.rpg.service';
-import { PlayersSurvivalService } from './services/players.survival.service';
+import { PlayersService } from '@/modules/players/services/players.service';
 import { RpgPrismaModule } from '@/databases/rpg/rpg-prisma.module';
 import { SurvivalPrismaModule } from '@/databases/survival/survival-prisma.module';
 
 @Module({
-  controllers: [
-    PlayersController,
-    PlayersRPGController,
-    PlayersSurvivalController,
-  ],
-  providers: [PlayersService, PlayersRPGService, PlayersSurvivalService],
-  exports: [PlayersService, PlayersRPGService, PlayersSurvivalService],
+  controllers: [],
+  providers: [PlayersService],
+  exports: [PlayersService],
   imports: [RpgPrismaModule, SurvivalPrismaModule],
 })
 export class PlayersModule {}
